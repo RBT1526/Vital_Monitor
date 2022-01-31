b2b6a32da449e491c464b837f750e81b0c06859d
RBT15 <gerfregosoj@hotmail.com>
2022-01-31 09:42:26 -0700

    Esp8622 firebase example code finished

--- test/Esp8266_firebase/Esp8266_firebase.ino
+++ test/Esp8266_firebase/Esp8266_firebase.ino
@@ -1,24 +1,16 @@
-
-/**
- * Created by K. Suwatchai (Mobizt)
- * 
- * Email: k_suwatchai@hotmail.com
- * 
- * Github: https://github.com/mobizt/Firebase-ESP-Client
- * 
- * Copyright (c) 2022 mobizt
- *
-*/
-
-/** This example will show how to authenticate using 
- * the legacy token or database secret with the new APIs (using config and auth data).
-*/
-
-#if defined(ESP32)
-#include <WiFi.h>
-#elif defined(ESP8266)
+// authenticate using the legacy token or database secret firebase example esp8266
+//
+//
+//
+//Gerardo Fregoso Jiménez Code based on firebase library
+//
+//
+//
+//
+//
+//---------––---------------------------------------------------------
 #include <ESP8266WiFi.h>
-#endif
+
 
 #include <Firebase_ESP_Client.h>
 
@@ -33,6 +25,7 @@
 #define DATABASE_URL "https://medicproyect-d2db9-default-rtdb.firebaseio.com/" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
 #define DATABASE_SECRET "h2lngwx8bLp73YE0C7YfJ6Cttbu6wpksdd4zPs21"
 
+//---------––---------------------------------------------------------
 /* 3. Define the Firebase Data object */
 FirebaseData fbdo;
 
@@ -42,15 +35,17 @@ FirebaseAuth auth;
 /* Define the FirebaseConfig data for config data */
 FirebaseConfig config;
 
-unsigned long dataMillis = 0;
-int count = 0;
+unsigned long dataMillis = 0; //timekeeper
+int count = 0; // counter 
+String test = ""; //save string
 
+//---------––---------------------------------------------------------
 void setup()
 {
 
-    Serial.begin(115200);
+    Serial.begin(115200); // start serial with 115200 bps
 
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);  // try to connect to wifi
     Serial.print("Connecting to Wi-Fi");
     while (WiFi.status() != WL_CONNECTED)
     {
@@ -59,14 +54,11 @@ void setup()
     }
     Serial.println();
     Serial.print("Connected with IP: ");
-    Serial.println(WiFi.localIP());
+    Serial.println(WiFi.localIP()); // print ip of the device
     Serial.println();
 
-    Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+    Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION); // print firebase version
 
-    /* Assign the certificate file (optional) */
-    //config.cert.file = "/cert.cer";
-    //config.cert.file_storage = StorageType::FLASH;
 
     /* Assign the database URL and database secret(required) */
     config.database_url = DATABASE_URL;
@@ -76,18 +68,23 @@ void setup()
 
     /* Initialize the library with the Firebase authen and config */
     Firebase.begin(&config, &auth);
-
-    //Or use legacy authenticate method
-    //Firebase.begin(DATABASE_URL, DATABASE_SECRET);
 }
 
+//---------––---------------------------------------------------------
 void loop()
-{
+{   
+    //every 1 second
     if (millis() - dataMillis > 1000)
     {
         dataMillis = millis();
-        Firebase.RTDB.setInt(&fbdo, "/Medical_project/Gerardo /Datos/Bpm", count++);
-        Firebase.RTDB.setInt(&fbdo, "/Medical_project/Gerardo /Datos/Temp", count+=2);
-        Firebase.RTDB.setInt(&fbdo, "/Medical_project/Gerardo /Datos/Spo2", count++);
+        Firebase.RTDB.setInt(&fbdo, "/Medical_project/Gerardo /Datos/Bpm", count++); // send count  ++ to Bpm
+        Firebase.RTDB.setInt(&fbdo, "/Medical_project/Gerardo /Datos/Temp", count+=2);// send count  count =+2 to Temp
+        Firebase.RTDB.setInt(&fbdo, "/Medical_project/Gerardo /Datos/Spo2", count++);// send count  count ++ to Spo2
+        Firebase.RTDB.getString(&fbdo, "/Medical_project/Gerardo /Device", &test); // get the string of Device
+        Serial.println(test);
     }
+    
 }
+
+//---------––---------------------------------------------------------
+//Gerardo Fregoso Jiménez 2022
